fcdb6dc6da166dda4a636781773716f5dc1fa38d
Chinmay anand <chinmayanand896@gmail.com>
2020-02-15 01:17:31 +0530

    second initial commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component{
@@ -7,21 +6,22 @@ class App extends Component{
     super();
 
     this.state = {
-      string: 'Hello world',
-      temp: 'Hello world'
+      monsters: []
     }
   }
+
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({monsters: users}))
+  }
+
   render(){
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            {this.state.string}
-          </p>
-          <button onClick = {() => this.setState({string: 'hello world, meenachinmay!'})}>Change the text</button>
-          <button onClick = {() => this.setState({string: this.state.temp})}>Change the text back</button>
-        </header>
+        {
+          this.state.monsters.map(monster => <h1 key={ monster.id }>{ monster.name }</h1>)
+        }
       </div>
     );
   }
