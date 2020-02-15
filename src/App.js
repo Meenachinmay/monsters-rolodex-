6e69778d2b410b4a21002092d459d7db8f7bd056
Chinmay anand <chinmayanand896@gmail.com>
2020-02-15 09:44:44 +0530

    card design added, components added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import { CardList} from './components/card-list/card-list.component';
 
 class App extends Component{
   constructor(){
@@ -19,9 +20,7 @@ class App extends Component{
   render(){
     return (
       <div className="App">
-        {
-          this.state.monsters.map(monster => <h1 key={ monster.id }>{ monster.name }</h1>)
-        }
+        <CardList monsters={this.state.monsters} /> 
       </div>
     );
   }
---
+++ src/components/card/card.component.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Card = (props) => {
+    return (
+        <div>
+            <h1>{props.monster.name}</h1>
+        </div>
+    )
+}
